f516f7835980409719441573a572a0a17b1deafa
Ruben-dlCG <delacoba333@gmail.com>
2026-07-08 14:17:30 +0000

    chore: initialize pom.xml with core spring boot dependencies
    Configured Maven starters for Web, Data JPA, Thymeleaf, Security, and PostgreSQL to establish the structural runtime environment directly in Codespaces.

---
+++ test/JavaClass.java
@@ -0,0 +1,8 @@
+public class JavaClass{
+    public static void exito(){
+        System.out.println("Funciona!!");
+    }
+        public static void main(String [] args){
+            exito();
+    }
+}
