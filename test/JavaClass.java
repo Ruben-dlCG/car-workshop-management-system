7ee1b9ff1efc2c01a7fdacf3b6ad0c195d493389
Ruben-dlCG <delacoba333@gmail.com>
2026-07-09 07:36:06 +0000

    chore: delete temporary test class files and update .gitignore to ignore internal developer notes and test files

--- test/JavaClass.java
+++
@@ -1,8 +0,0 @@
-public class JavaClass{
-    public static void exito(){
-        System.out.println("Funciona!!");
-    }
-        public static void main(String [] args){
-            exito();
-    }
-}
